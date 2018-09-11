7899abf514c5a9b1c4a2dd6bc6b4f59068629df9
Ayana Bando <ayanaflax@yahoo.com>
2018-09-11 19:59:10 +0000

    update scrape class with initial row data

--- lib/room_scraper.rb
+++ lib/room_scraper.rb
@@ -1,9 +1,14 @@
 
 class RoomScraper
 
- #@doc.search("li p time") .attr("datetime")
+
   def initialize(index_url)
     @doc = Nokogiri::HTML(open(index_url))
     binding.pry
+  end
+
+  def rows
+    #@doc.search("li p time") .attr("datetime")
+    @rows ||= @doc.search("div.content li.result-row p.result-info")
   end
 end
