99c7bc69b609e96b953fccbec617b680c51f64c2
Ayana Bando <ayanaflax@yahoo.com>
2018-09-12 02:04:43 +0000

    update call and scrape_row method

--- lib/room_scraper.rb
+++ lib/room_scraper.rb
@@ -4,26 +4,24 @@ class RoomScraper
 
   def initialize(index_url)
     @doc = Nokogiri::HTML(open(index_url))
-    binding.pry
+    #binding.pry
   end
 
   def call
     rows.each do |row_doc|
-      #scrape_row(row_doc)
+      Room.create_from_hash(scrape_row(row_doc)) #should put room in my database
     end
   end
 
   private
   def rows
-    #@doc.search("li p time") .attr("datetime")
     @rows ||= @doc.search("div.content li.result-row p.result-info")
   end
 
   def scrape_row(row)
    #scrape an individual rows
-
   {
-    :date_created => row.search("time.result-date").text,
+    :date_created => row.search("time").attribute("datetime").text,
     :title => row.search("a.hdrlnk").text,
     :url => row.search("a.hdrlnk").attribute("href").text,
     :price => row.search("span.result-price").text
